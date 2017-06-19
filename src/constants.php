1e9af805e9483dccbca3b3c946e0951941d0b73f
Steve <sbarry50@gmail.com>
2017-06-19 16:09:09 -0400

    Add docblocks for constants functions

--- src/constants.php
+++ src/constants.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Constants - define the plugin's magic constants
+ * Constants - Defines the plugin's magic constants
  *
  * @package     Vendor_Name\Plugin_Name
  * @since       1.0.0
@@ -11,6 +11,12 @@
 
  namespace Vendor_Name\Plugin_Name;
 
+ /**
+  * Defines the plugin's magic constants
+  *
+  * @since  1.0.0
+  * @param  string    $plugin_root_file The root file of the plugin
+  */
 function define_plugin_constants( $plugin_root_file ) {
 
     $const = get_plugin_constants( $plugin_root_file );
@@ -22,6 +28,13 @@ function define_plugin_constants( $plugin_root_file ) {
     }
 }
 
+/**
+ * Gets the array of plugin constants and returns them
+ *
+ * @since  1.0.0
+ * @param  string    $plugin_root_file The root file of the plugin
+ * @return array                       Array of plugin constants
+ */
 function get_plugin_constants( $plugin_root_file ) {
 
     $plugin_data = get_plugin_data( $plugin_root_file );
@@ -35,6 +48,13 @@ function get_plugin_constants( $plugin_root_file ) {
     );
 }
 
+/**
+ * Gets the plugin URL
+ *
+ * @since  1.0.0
+ * @param  string    $plugin_root_file The root file of the plugin
+ * @return string    $plugin_url       The plugin URL
+ */
 function get_plugin_url( $plugin_root_file ) {
     $plugin_url = plugin_dir_url( $plugin_root_file );
     if ( is_ssl() ) {
@@ -43,6 +63,13 @@ function get_plugin_url( $plugin_root_file ) {
     return $plugin_url;
 }
 
+/**
+ * Gets plugin data from the plugin's bootstrap file header comment using WP core's get_plugin_data function
+ *
+ * @since  1.0.0
+ * @param  string    $plugin_root_file The root file of the plugin
+ * @return array                       Array of plugin data from the bootstrap file header comment
+ */
 function get_plugin_data( $plugin_root_file ) {
     if ( !function_exists( 'get_plugin_data' ) ) {
         require_once ABSPATH . '/wp-admin/includes/plugin.php';
